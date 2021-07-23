88c12e7fb1ff09bf5f29739c5aaff612db8c178c
Rita Carvalho <ritacarvalho@Ritas-MBP.lan>
2021-07-23 13:20:05 +0100

    ADD: Heading and BodyText styled components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Homepage from "./Components/Homepage/Homepage";
 import Products from "./Components/Products/Products";
 import {Button , BrownButton, BlueButton} from "./Components/Button/Button";
 import ShopMore from "./Components/ShopMore/ShopMore";
-
+import Ingredients from "./Components/Ingredients/Ingredients";
+import {Heading2} from "./Components/Heading/Heading";
 
 function App() {
   return (
@@ -12,9 +13,11 @@ function App() {
     <Homepage></Homepage>
     <Products></Products>
     <ShopMore></ShopMore>
+    <Ingredients></Ingredients>
     <Button>Shop products</Button>
     <BrownButton>Subscribe</BrownButton>
     <BlueButton>Continue to Review Order</BlueButton>
+    <Heading2>Heading 2</Heading2>
     </Layout>
   );
 }
