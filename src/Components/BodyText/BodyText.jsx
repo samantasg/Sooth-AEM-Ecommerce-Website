6d37f2ba02e24697c58e6c79d122b7e4f230074d
Rita Carvalho <ritacarvalho@Ritas-MBP.lan>
2021-07-23 17:27:44 +0100

    UPD: Ingredients and ShopMore components ADD: Input styled component

--- src/Components/BodyText/BodyText.jsx
+++ src/Components/BodyText/BodyText.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const BodyText = styled.p`
-color: #354E57;
+color: ${props => props.BodyTextColor || "#354E57"};
 font-size: 1.4rem;
 line-height: 1.7rem; 
 `;
