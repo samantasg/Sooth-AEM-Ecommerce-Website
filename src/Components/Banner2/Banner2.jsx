0e3ad4ce23fb67f1260705bf019a053e1d143aed
aldo <aldo@studiographene.com>
2021-08-05 15:01:40 +0100

    creating conflicts

--- src/Components/Banner2/Banner2.jsx
+++ src/Components/Banner2/Banner2.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import {Heading4} from "../Heading/Heading";
-import {BodyText4} from "../BodyText/BodyText";
+import { Heading4 } from "../Heading/Heading";
+import { BodyText4 } from "../BodyText/BodyText";
 
 function Banner2() {
-	return (
-		<div className="banner-2">
-            <Heading4 HeadingColor="#354E57">Solutions for all skin</Heading4>
-            <BodyText4 BodyTextColor="#354E57">Explore our innovative skincare products</BodyText4>
-        </div>
-	);
+  return (
+    <div className="banner-2">
+      <Heading4 HeadingColor="#354E57">Solutions for all </Heading4>
+      <BodyText4 BodyTextColor="#354E57">
+        Explore our innovative skincare products
+      </BodyText4>
+    </div>
+  );
 }
 
 export default Banner2;
