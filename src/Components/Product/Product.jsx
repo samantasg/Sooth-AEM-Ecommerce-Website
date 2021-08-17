0ad363a2d413e2cccc6ec5f8ae90a2cc73609d69
aldo <aldo@studiographene.com>
2021-08-17 16:08:11 +0100

    product page render fix

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,31 +1,36 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import Button from "../Button/Button";
-import {items} from "../Mockups/items";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import Button from '../Button/Button';
+import { items } from '../Mockups/items';
 
 function Product(props) {
-    return (
-        <div className="product">
-            {items.map(product => (
-            <Link to={`/product/${product.name}`}>
-                <div className="product-bg-img" style={{height: (props.height)}}></div>
-            {/* <div className="product-stamp">
+  return (
+    <div className="product">
+      {items.map(product => (
+        <Link to={`/product/${product.slug}`}>
+          <div className="product-bg-img" style={{ height: props.height }}></div>
+          {/* <div className="product-stamp">
                 <p>New!</p>
             </div> */}
-            <div className="product-info">
-                <p>{product.name}</p>
-                <p>{product.info}</p>
-                <p>{product.price}</p>
-            </div>
+          <div className="product-info">
+            <p>{product.name}</p>
+            <p>{product.info}</p>
+            <p>{product.price}</p>
+          </div>
         </Link>
-            )
-            )
-            }
-            <Button fullWidth><FontAwesomeIcon color='#2E2E22' icon={faShoppingCart} style={{marginRight: 12}}></FontAwesomeIcon>Add to cart </Button>
-        </div>
-    );
+      ))}
+      <Button fullWidth>
+        <FontAwesomeIcon
+          color="#2E2E22"
+          icon={faShoppingCart}
+          style={{ marginRight: 12 }}
+        ></FontAwesomeIcon>
+        Add to cart
+      </Button>
+    </div>
+  );
 }
 
 export default Product;
